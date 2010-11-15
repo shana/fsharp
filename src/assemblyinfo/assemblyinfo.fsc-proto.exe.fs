bb2ec10dd476ccf91529fd61f55a158dd854b65b
Andreia Gaita <shana@jitted.com>
2010-11-15 12:40:07 +0000

    Initial commit

---
+++ src/assemblyinfo/assemblyinfo.fsc-proto.exe.fs
@@ -0,0 +1,3 @@
+namespace Microsoft.BuildSettings
+                [<System.Runtime.Versioning.TargetFrameworkAttribute(".NETFramework,Version=v4.0", FrameworkDisplayName=".NET Framework 4")>]
+                do ()
